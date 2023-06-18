c23b53cd2d41eeb3230c74fbb18404285ed124ff
jeessh <jesshuang5733@gmail.com>
2023-06-18 13:50:31 -0400

    first commit

---
+++ src/Pages/Home/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../Components/navbar/navbar';
+import Hero from '../../Components/hero/hero';
+
+const Home = () => {
+  return (
+    <>
+        <Navbar/>
+        <Hero/>
+    </>
+  )
+}
+
+export default Home
